a9825b23a4b23005955471b4c3535541d2517aee
iFlintksks <rafaelnuneszeiro2@gmail.com>
2025-02-17 09:24:49 -0400

    Update index.php

--- Public/index.php
+++ Public/index.php
@@ -83,6 +83,5 @@ echo '<hr>Ultimo ID inserido'.$db->ultimoIdInserido();
     ?>
     <script src="<?=URL?>/public/bootstrap/js/bootstrap.js"></script>
     <script src="<?=URL?>/public/js/query.js"></script>
-    gfhjgdcf yi
 </body>
 </html>
